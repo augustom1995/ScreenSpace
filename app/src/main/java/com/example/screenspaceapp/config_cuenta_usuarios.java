0f769f1691431079a5cd88b134776268404dccb9
Augusto <mancuello95augusto@hotmail.com>
2023-05-28 04:03:49 -0300

    perfil de usuario front updated

---
+++ app/src/main/java/com/example/screenspaceapp/config_cuenta_usuarios.java
@@ -0,0 +1,14 @@
+package com.example.screenspaceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class config_cuenta_usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_cuenta_usuarios);
+    }
+}
---
+++ app/src/main/java/com/example/screenspaceapp/exito_modificar_cuenta.java
@@ -0,0 +1,14 @@
+package com.example.screenspaceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exito_modificar_cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exito_modificar_cuenta);
+    }
+}
